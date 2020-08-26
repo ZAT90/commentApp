94fd3b12f4e3468ccda11a9d519fac739c57aec3
ZAT90 <bdzebel@gmail.com>
2020-08-26 21:18:25 +0800

    bloc being added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import './splash.dart';
 import './home.dart';
+import 'bloc/setup/blocObserver.dart';
 
 void main() {
+  Bloc.observer = SimpleBlocObserver();
   runApp(MyApp());
 }
 
